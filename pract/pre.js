20d33830fddb1940b33618d648d8ffcac2eb172b
syam-00 <syambabu071@gmail.com>
2025-05-06 23:23:15 +0530

    Add files via upload

---
+++ pract/pre.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    document.getElementById('preloader').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+    document.body.style.overflow = 'auto';
+  });
+  
